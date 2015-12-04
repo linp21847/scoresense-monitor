e7c202a193fd57420e8971266f6d029b5fd14924
Paul <linp21847@gmail.com>
2015-12-04 18:06:02 +0100

    not exporting issue for roy account

--- assets/js/content_script.js
+++ assets/js/content_script.js
@@ -135,79 +135,78 @@ chrome.extension.sendMessage({msg: "state"}, function(param) {
 							Experian: $records[1].textContent.trim(),
 							Equifax: $records[2].textContent.trim()
 						};
-					};
+					},
+					extractLatePayment = function($container) {
+							var $internalRecords = $container.find("tr tr"),
+								$imagesContainer = $($internalRecords[0]).find("td").slice(2),
+								$dateInfoContainer = $($internalRecords[1]).find("td").slice(1),
+								$allImgs = $imagesContainer.find("img"),
+								$allMonths = $dateInfoContainer.children("img:first-child"),
+								$late30Imgs = $imagesContainer.find("img[src='/OTProductWeb/resources/base/en/images/creditreport/tradeline-30.gif']"),
+								$late60Imgs = $imagesContainer.find("img[src='/OTProductWeb/resources/base/en/images/creditreport/tradeline-60.gif']"),
+								$late90Imgs = $imagesContainer.find("img[src='/OTProductWeb/resources/base/en/images/creditreport/tradeline-90.gif']"),
+								latePaymentDates = {
+									30: "",
+									60: "",
+									90: ""
+								},
+								months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"],
+								parseYear = function(index) {
+									var $imgs = $($dateInfoContainer[index]).find("img").slice(1),
+										year = "20";
 
-				extractLatePayment = function($container) {
-					var $internalRecords = $container.find("tr tr"),
-						$imagesContainer = $($internalRecords[0]).find("td").slice(2),
-						$dateInfoContainer = $($internalRecords[1]).find("td").slice(1),
-						$allImgs = $imagesContainer.find("img"),
-						$allMonths = $dateInfoContainer.children("img:first-child"),
-						$late30Imgs = $imagesContainer.find("img[src='/OTProductWeb/resources/base/en/images/creditreport/tradeline-30.gif']"),
-						$late60Imgs = $imagesContainer.find("img[src='/OTProductWeb/resources/base/en/images/creditreport/tradeline-60.gif']"),
-						$late90Imgs = $imagesContainer.find("img[src='/OTProductWeb/resources/base/en/images/creditreport/tradeline-90.gif']"),
-						latePaymentDates = {
-							30: "",
-							60: "",
-							90: ""
-						},
-						months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"],
-						parseYear = function(index) {
-							var $imgs = $($dateInfoContainer[index]).find("img").slice(1),
-								year = "20";
+									for (var yearIndex = 0; yearIndex < $imgs.length; yearIndex ++) {
+										year += $($imgs[yearIndex]).attr('alt');
+									}
+									return year;
+								},
+								getYearFromInd = function(index) {
+									var $yearImages = $allMonths.parent().find("img[src='/OTProductWeb/resources/base/en/images/creditreport/monthsandyears/apostrophe-gray.gif']"),
+										firstYearIndex = $dateInfoContainer.index($($yearImages[0]).parent()[0]),
+										secondYearIndex = $dateInfoContainer.index($($yearImages[1]).parent()[0]),
+										year = "";
 
-							for (var yearIndex = 0; yearIndex < $imgs.length; yearIndex ++) {
-								year += $($imgs[yearIndex]).attr('alt');
+									if (index > 11) {
+										year = parseInt(parseYear(firstYearIndex));
+									} else {
+										year = parseInt(parseYear(secondYearIndex)) - 1;
+									}
+									return year;
+								};
+
+							if ($late30Imgs.length === 0 && $late60Imgs.length === 0 && $late90Imgs.length === 0) {
+								return latePaymentDates;
 							}
-							return year;
-						},
-						getYearFromInd = function(index) {
-							var $yearImages = $allMonths.parent().find("img[src='/OTProductWeb/resources/base/en/images/creditreport/monthsandyears/apostrophe-gray.gif']"),
-								firstYearIndex = $dateInfoContainer.index($($yearImages[0]).parent()[0]),
-								secondYearIndex = $dateInfoContainer.index($($yearImages[1]).parent()[0]),
-								year = "";
 
-							if (index > 11) {
-								year = parseInt(parseYear(firstYearIndex));
-							} else {
-								year = parseInt(parseYear(secondYearIndex)) - 1;
+							if ($late30Imgs.length > 0) {
+								var ind = $imagesContainer.index($($late30Imgs[0]).parent());
+								latePaymentDates['30'] = $($allMonths[ind]).attr('alt');
+								if (months.indexOf(latePaymentDates['30']) === -1) {
+									latePaymentDates['30'] = months[0];
+								}
+								latePaymentDates['30'] += '-' + getYearFromInd(ind);
 							}
-							return year;
+
+							if ($late60Imgs.length > 0) {
+								var ind = $imagesContainer.index($($late60Imgs[0]).parent());
+								latePaymentDates['60'] = $($allMonths[ind]).attr('alt');
+								if (months.indexOf(latePaymentDates['60']) === -1) {
+									latePaymentDates['60'] = months[0];
+								}
+								latePaymentDates['60'] += '-' + getYearFromInd(ind);
+							}
+
+							if ($late90Imgs.length > 0) {
+								var ind = $imagesContainer.index($($late90Imgs[0]).parent());
+								latePaymentDates['90'] = $($allMonths[ind]).attr('alt');
+								if (months.indexOf(latePaymentDates['90']) === -1) {
+									latePaymentDates['90'] = months[0];
+								}
+								latePaymentDates['90'] += '-' + getYearFromInd(ind);
+							}
+
+							return latePaymentDates;
 						};
-
-					if ($late30Imgs.length === 0 && $late60Imgs.length === 0 && $late90Imgs.length === 0) {
-						return latePaymentDates;
-					}
-
-					if ($late30Imgs.length > 0) {
-						var ind = $imagesContainer.index($($late30Imgs[0]).parent());
-						latePaymentDates['30'] = $($allMonths[ind]).attr('alt');
-						if (months.indexOf(latePaymentDates['30']) === -1) {
-							latePaymentDates['30'] = months[0];
-						}
-						latePaymentDates['30'] += '-' + getYearFromInd(ind);
-					}
-
-					if ($late60Imgs.length > 0) {
-						var ind = $imagesContainer.index($($late60Imgs[0]).parent());
-						latePaymentDates['60'] = $($allMonths[ind]).attr('alt');
-						if (months.indexOf(latePaymentDates['60']) === -1) {
-							latePaymentDates['60'] = months[0];
-						}
-						latePaymentDates['60'] += '-' + getYearFromInd(ind);
-					}
-
-					if ($late90Imgs.length > 0) {
-						var ind = $imagesContainer.index($($late90Imgs[0]).parent());
-						latePaymentDates['90'] = $($allMonths[ind]).attr('alt');
-						if (months.indexOf(latePaymentDates['90']) === -1) {
-							latePaymentDates['90'] = months[0];
-						}
-						latePaymentDates['90'] += '-' + getYearFromInd(ind);
-					}
-
-					return latePaymentDates;
-				};
 
 				var collectionTableFlag = false;
 
@@ -257,6 +256,10 @@ chrome.extension.sendMessage({msg: "state"}, function(param) {
 										""
 									]
 								};
+
+								if (!tempAccount.name && !tempAccount.detailViewLink)
+									break;
+								
 								accounts.push(tempAccount);
 								i++;
 
